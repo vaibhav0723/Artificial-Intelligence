d55f6f86772a9a113646dae82dbbdc1ef5dd9839
vaibhav0723 <166782729+vaibhav0723@users.noreply.github.com>
2024-10-15 22:14:00 +0530

    Add files via upload

---
+++ DL/Assignment-2/Assignment-2.py
@@ -0,0 +1,94 @@
+# plot photo with detected faces using opencv cascade classifier
+from cv2 import imread
+from cv2 import imshow
+from cv2 import waitKey
+from cv2 import destroyAllWindows
+from cv2 import CascadeClassifier
+from cv2 import rectangle
+# load the photograph
+pixels = imread('faces.jpeg')
+
+# load the pre-trained modelz
+classifier = CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# perform face detection
+bboxes = classifier.detectMultiScale(pixels)
+
+# print bounding box for each detected face
+for box in bboxes:
+	# extract
+	x, y, width, height = box
+	x2, y2 = x + width, y + height
+	# draw a rectangle over the pixels
+	rectangle(pixels, (x, y), (x2, y2), (0,0,255), 1)
+# show the image
+imshow('face detection', pixels)
+# keep the window open until we press a key
+waitKey(0)
+# close the window
+destroyAllWindows()
+
+
+
+
+# plot photo with detected faces using opencv cascade classifier
+from cv2 import imread
+from cv2 import imshow
+from cv2 import waitKey
+from cv2 import destroyAllWindows
+from cv2 import CascadeClassifier
+from cv2 import rectangle
+# load the photograph
+pixels = imread('berkeley_faces.jpg')
+
+# load the pre-trained modelz
+classifier = CascadeClassifier('haarcascade_frontalface_default.xml')
+# perform face detection
+bboxes = classifier.detectMultiScale(pixels)
+# print bounding box for each detected face
+for box in bboxes:
+	# extract
+	x, y, width, height = box
+	x2, y2 = x + width, y + height
+	# draw a rectangle over the pixels
+	rectangle(pixels, (x, y), (x2, y2), (0,0,255), 1)
+# show the image
+imshow('face detection', pixels)
+# keep the window open until we press a key
+waitKey(0)
+# close the window
+destroyAllWindows()
+
+
+
+
+
+# plot photo with detected faces using opencv cascade classifier
+from cv2 import imread
+from cv2 import imshow
+from cv2 import waitKey
+from cv2 import destroyAllWindows
+from cv2 import CascadeClassifier
+from cv2 import rectangle
+# load the photograph
+pixels = imread('pexels-photo-2379005.jpeg')
+
+# load the pre-trained modelz
+classifier = CascadeClassifier('haarcascade_frontalface_default.xml')
+# perform face detection
+bboxes = classifier.detectMultiScale(pixels)
+# print bounding box for each detected face
+for box in bboxes:
+	# extract
+	x, y, width, height = box
+	x2, y2 = x + width, y + height
+	# draw a rectangle over the pixels
+	rectangle(pixels, (x, y), (x2, y2), (0,0,255), 1)
+# show the image
+imshow('face detection', pixels)
+# keep the window open until we press a key
+waitKey(0)
+# close the window
+destroyAllWindows()
+# plot photo with detected faces using opencv cascade classifier
+
